13bdc9af3a40907d7f0af757956793cbf8bb38ed
NevoEldad <nevoeldad@gmail.com>
2021-02-27 22:54:21 +0200

    before compoenent

---
+++ vue-cli-01/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <h2>Friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    // friends: [
+    //   { id: 123, name: "Eli", phone: "055555555", email: "Eli@eliserver.com" },
+    //   { id: 123, name: "Eli", phone: "055555555", email: "Eli@eliserver.com" },
+    //   { id: 123, name: "Eli", phone: "055555555", email: "Eli@eliserver.com" }
+    // ];
+  }
+};
+</script>
---
+++ vue-cli-01/src/components/FriendContact.vue
@@ -0,0 +1,38 @@
+<template>
+  <li>
+    <h2>{{friend.name}}</h2>
+    <button>Show/Hide</button>
+    <ul>
+      <li>
+        <strong>Phone:</strong>
+        {{friend.phone}}
+      </li>
+      <li>
+        <strong>EMail:</strong>
+        {{friend.email}}
+        }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: 123,
+        name: "Eli",
+        phone: 055555555,
+        email: "Eli@eliserver.com"
+      }
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+};
+</script>
