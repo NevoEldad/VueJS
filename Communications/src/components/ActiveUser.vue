ddb74bf853b781115971588631a450579416d253
NevoEldad <nevoeldad@gmail.com>
2021-03-05 11:44:55 +0200

    communications

---
+++ Communications/src/components/ActiveUser.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h3>{{this.nameShown}}</h3>
+    <h3>{{this.ageShown}}</h3>
+  </div>
+</template>
+<script>
+export default {
+  props: ["nameShown", "ageShown"],
+  data() {
+    return {};
+  }
+};
+</script>
---
+++ Communications/src/components/UserData.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    name:
+    <input type="text" v-model="name" />
+    {{namee}}
+    age:
+    <input type="text" v-model="age" />
+    {{agee}}
+    <button @click="addUser">Add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { age: "", name: "" };
+  },
+  computed: {
+    agee() {
+      return this.age;
+    },
+    namee() {
+      return this.name;
+    }
+  },
+  methods: {
+    addUser() {
+      this.$emit("add-user", { name: this.namee, age: this.agee });
+      this.age = "";
+      this.name = "";
+    }
+  }
+};
+</script>
