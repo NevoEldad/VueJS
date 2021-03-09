90906df35ea8f9f964499c793e3d767ce299913f
NevoEldad <nevoeldad@gmail.com>
2021-03-09 19:51:19 +0200

    some changes

--- components-advanced/src/App.vue
+++ components-advanced/src/App.vue
@@ -1,25 +1,21 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
   </div>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
-import UserInfo from "./components/UserInfo.vue";
-import BadgeList from "./components/BadgeList.vue";
+// import UserInfo from "./components/UserInfo.vue";
+// import BadgeList from "./components/BadgeList.vue";
+// import CourseGoal from "./components/CourseGoals.vue";
 
 export default {
   components: {
-    TheHeader,
-    BadgeList,
-    UserInfo
+    TheHeader
+    // BadgeList,
+    // UserInfo,
+    // CourseGoal
   },
   data() {
     return {
---
+++ components-advanced/src/components/CourseGoals.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <li v-for="goal in goals" :key="goal">
+      {{goal}}>
+      <slot :item="goal" some-other="some-other"></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      goals: ["finish", "bla"]
+    };
+  }
+};
+</script>
